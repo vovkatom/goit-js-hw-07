1802a54b8eff872fe66e5a528b610a8ee23dbb58
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-09-19 15:31:31 +0300

    less1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+  galleryImage.setAttribute("data-source", item.original);
+
+  // Додаємо обробник події click на кожен елемент зображення
+
+  galleryLink.addEventListener("click", (e) => {
+    e.preventDefault(); // Забороняємо стандартну дію (перенаправлення)
+    const lightbox = basicLightbox.create(
+      `<img src="${item.original}" alt=item.description>`
+    );
+    lightbox.show(); // Відкриваємо модальне вікно з зображенням
+  });
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+
+function createGallery(items) {
+  const galleryFragment = document.createDocumentFragment();
+  items.forEach((item) => {
+    const galleryItem = createGalleryItem(item);
+    galleryFragment.appendChild(galleryItem);
+  });
+  galleryList.appendChild(galleryFragment);
+}
+
+createGallery(galleryItems);
