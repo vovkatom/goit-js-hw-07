27f39e8c15d7ef736fb700196bf4053c09da4d37
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-09-19 17:13:53 +0300

    less2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+
+function createGallery(items) {
+  const galleryFragment = document.createDocumentFragment();
+  items.forEach((item) => {
+    const galleryItem = createGalleryItem(item);
+    galleryFragment.appendChild(galleryItem);
+  });
+  galleryList.appendChild(galleryFragment);
+}
+
+createGallery(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+  alertError: false,
+  captionsData: "alt",
+  fileExt: "png|jpg|jpeg|gif",
+  animationSpeed: 50,
+  showCounter: true,
+  captionDelay: 250,
+  preloading: false,
+});
